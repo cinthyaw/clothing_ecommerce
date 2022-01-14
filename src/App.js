ded7ec39c06d53b1d772c1e8383ad392f23614fa
cinthya-gm <cinthya.gutierrez@magmalabs.io>
2022-01-14 16:02:51 -0600

    changes for router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import HomePage from './pages/homepage.component';
+import { Route } from 'react-router-dom';
+
+const HatsPage = ()=> (
+  <div>
+    <h1>hats page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+        <Route component={HomePage}></Route>
     </div>
   );
 }
