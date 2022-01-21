1479e559d3127352a6951bac8bc48e003d6916c6
cinthya-gm <cinthya.gutierrez@magmalabs.io>
2022-01-21 15:23:33 -0600

    added user types file redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import HomePage from './pages/homepage.component';
-import { Route, Switch} from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInSignUp from './components/sign-in-and-sign-up/sign-in.component';
 import { auth, createUserProfileDocument} from './firebase/firebase.utils'
 import React from 'react';
 import { connect } from "react-redux";
-import { setCurrentUser } from "./redux/user/user.action"
+import { setCurrentUser } from "./redux/user/user.action";
+
 
 
 class App extends React.Component {
@@ -46,7 +47,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' component={HomePage}></Route>
             <Route path="/shop" component={ShopPage}></Route>
-            <Route path="/signin" component={SignInSignUp}></Route>
+            <Route exact path="/signin" render={()=> this.props.currentUser ? (<Redirect to="/"></Redirect>) : (<SignInSignUp></SignInSignUp>)}></Route>
             </Switch>
         </div>
       );
@@ -54,8 +55,12 @@ class App extends React.Component {
   
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
