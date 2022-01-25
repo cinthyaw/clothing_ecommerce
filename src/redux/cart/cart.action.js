8a55e9fce547269362df6767b53ca7597bebb5ce
cinthya-gm <cinthya.gutierrez@magmalabs.io>
2022-01-25 16:28:11 -0600

    adding funcionality to add an item to the cart

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -4,4 +4,9 @@ export const tooggleCartHidden = ()=>({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
 
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+});
+
 
