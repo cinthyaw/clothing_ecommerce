3a7b95bc5f0960d302b084b2792b525b411beb5c
cinthya-gm <cinthya.gutierrez@magmalabs.io>
2022-01-27 14:22:46 -0600

    adding changes in dropdown component

--- src/components/cart/cart-dropdown.component.jsx
+++ src/components/cart/cart-dropdown.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
-import "./cart-dropdown.styles.scss"
+import "./cart-dropdown.styles.scss";
+import CartItem from "../cart-item/cart-item.component";
 
 const Cart = ()=> (
     <div className="cart-dropdown">
         <div class className="cart-items">
+            <CartItem />
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     </div>
